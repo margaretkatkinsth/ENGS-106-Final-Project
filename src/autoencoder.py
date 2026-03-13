bca993ff5c3d164adbbb67fbc0680383d57cf49e
aidancurtis <aicurtis@pm.me>
2026-03-13 10:16:06 -0400

    update autoencoder training

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -173,29 +173,6 @@ class SpatialVAE(nn.Module):
         return recon, mean, logvar, z
 
 
-class PatchDiscriminator(nn.Module):
-    """
-    PatchGAN discriminator
-    """
-
-    def __init__(self, in_ch=1):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(in_ch, 64, 4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(64, 128, 4, stride=2, padding=1, bias=False),
-            nn.GroupNorm(8, 128),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=False),
-            nn.GroupNorm(16, 256),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(256, 1, 4, padding=1),
-        )
-
-    def forward(self, x):
-        return self.net(x)
-
-
 class SharpReconLoss(nn.Module):
     lap_kernel: torch.Tensor
     sobel_x: torch.Tensor
